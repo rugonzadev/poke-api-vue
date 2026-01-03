3625efe39503378bde81b1c8a497c445024982e4
Rubén <rubengnz00@gmail.com>
2026-01-03 20:12:55 +0100

    fix: display multiple Pokémon forms / style: shiny hover effect and improved card layout

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,35 @@ import CardComponent from './components/CardComponent.vue';
 
 let pokemonName = ref("")
 let pokemonList = ref([])
+
 async function findPoke() {
     const url = "https://pokeapi.co/api/v2/pokemon/"
     const urlFallBack = "https://pokeapi.co/api/v2/pokemon-species/"
     try{
-        let response = await fetch(url+pokemonName.value);
+        let response = await fetch(url+pokemonName.value.toLowerCase().trim());
         let myData = await response.json();
-        pokemonList.value.push(myData);
+        let poke =[]
+        poke.push(myData)
+        pokemonList.value.push(poke);
     }catch (error){
-        try{
-            let response1 = await fetch(urlFallBack+pokemonName.value);
-        let myData = await response1.json();
-        let response2 = await fetch(myData.varieties[0].pokemon.url)
-        let myData2 = await response2.json();
-        pokemonList.value.push(myData2);
-        } catch (error){
-            console.error("Poke not found")
+        let response1 = await fetch(urlFallBack + pokemonName.value.toLowerCase().trim());
+        let myData1 = await response1.json();
+
+        let poke = [];
+
+        for (let form of myData1.varieties) {
+            const response2 = await fetch(form.pokemon.url);
+            const myData2 = await response2.json();
+            poke.push(myData2);
         }
+
+        pokemonList.value.push(poke);
         
     }
+}
+
+function resetPoke(){
+    pokemonList.value = []
 }
 
 </script> 
@@ -34,7 +44,8 @@ async function findPoke() {
     <fieldset>
         <label for="pokemonName">Pokemon Name</label>
         <input type="text" id="pokemonName" v-model.lazy="pokemonName" placeholder="write here the Pokemon name">
-        <button @click="findPoke">Search!</button>
+        <button @click="findPoke" id="search">Search!</button>
+        <button @click="resetPoke" id="reset">Reset!</button>
     </fieldset>
     
     <div id="gallery">
@@ -43,24 +54,65 @@ async function findPoke() {
 </template>
 
 <style>
-    fieldset{
-        background-color: white;
-        border-radius: 20px;
-        border: 1px solid black;
-        display: flex;
-        flex-direction: column;
-        width: 50vw;
-        justify-self: center;
-        text-align: center;
-        padding: 2vh;
-        margin-bottom: 3vh;
-    }
-
     #gallery{
         display: flex;
         gap: 30px;
         flex-wrap: wrap;
         flex-direction: row;
         justify-content: space-around;
+    }
+
+    fieldset {
+    background-color: white;
+    border-radius: 20px;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    width: 50vw;
+    justify-self: center;
+    text-align: center;
+    padding: 2vh;
+    gap: 1rem;
+    margin-bottom: 3vh;
+}
+
+    input {
+        padding: 0.5rem 1rem;
+        border-radius: 10px;
+        border: 1px solid #ccc;
+        font-size: 1rem;
+        outline: none;
+        transition: border 0.2s;
+    }
+
+    input:focus {
+        border-color: #007bff;
+    }
+
+    button {
+        padding: 0.6rem 1.2rem;
+        border-radius: 12px;
+        border: none;
+        cursor: pointer;
+        font-weight: bold;
+        font-size: 1rem;
+        color:black;
+        background: wheat;
+    }
+
+    #search:hover {
+        box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+        background: greenyellow;
+    }
+
+    #reset:hover {
+        box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+        color: rgb(228, 56, 56);
+        background: rgb(255, 216, 43);
+    }
+
+    button:active {
+        transform: scale(0.9);
+        box-shadow: 0 2px 8px rgba(0,0,0,0.2);
     }
 </style>
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,24 +1,61 @@
 <script setup>
-    defineProps({
-        pokemon: Object
-    })
+import { ref } from 'vue'
+
+const props = defineProps({
+    pokemon: Array
+})
+
+// Hacemos una copia reactiva
+const localPokemon = ref(
+    props.pokemon.map(f => ({
+    ...f, 
+    currentImg: f.sprites.front_default,
+    nameCapitalized: f.name.charAt(0).toUpperCase()+ f.name.slice(1)
+    }))
+)
+
+// Función para cambiar a shiny
+function shiny(f) {
+    f.currentImg = f.sprites.front_shiny
+}
+
+// Función para volver a normal
+function normal(f) {
+    f.currentImg = f.sprites.front_default
+}
 </script>
 
 <template>
     <div class="card">
-        <img :src="pokemon.sprites.front_default">
-        <p>{{ pokemon.name }}</p>
+        <div v-for="(f, index) in localPokemon" :key="index" @mouseenter="shiny(f)" @mouseleave="normal(f)">
+            <img :src="f.currentImg" :alt="f.name" />
+            <span>{{ f.nameCapitalized }}</span>
+        </div>
     </div>
 </template>
 
+
 <style>
-    .card{
-        display: flex;
-        background-color: antiquewhite;
-        align-items: center;
-        justify-content: center;
-        width: 35%;
-        border: 1px solid black;
-        border-radius: 10px;
-    }
+    .card {
+    display: flex;
+    gap: 10px;
+    background-color: antiquewhite;
+    padding: 10px;
+    border: 1px solid black;
+    border-radius: 10px;
+    flex-wrap: wrap;
+}
+
+.card div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+}
+
+.card div:hover img{
+    transform: scale(1.2);
+}
+
 </style>
